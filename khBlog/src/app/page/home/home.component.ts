78d0b82bf472fc342f8218151dd933d4f4fe67e6
jungle <dgjungle_c@163.com>
2021-11-24 00:27:32 +0800

    init

--- khBlog/src/app/page/home/home.component.ts
+++ khBlog/src/app/page/home/home.component.ts
@@ -1,15 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.less']
+  styleUrls: ['./home.component.less'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  public articleList: Array<any> = [];
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.http
+      .get(`http://localhost:8090/index/article`)
+      .subscribe((data: any) => {
+        debugger;
+        this.articleList = data.data;
+      });
   }
-
 }
